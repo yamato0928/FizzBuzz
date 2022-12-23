509fde4d9cb99f827680f67b9db8417db35d10ea
TomooYamaguchi <121102242+yamato0928@users.noreply.github.com>
2022-12-23 21:03:12 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,13 +4,16 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		for (int i=1;i<=100;i++) {
+			//3で割り切れるかつ5で割り切れる
 			if(((i%3)==0) && ((i%5)==0)) {
 				System.out.println("FizzBuzz");
 			}
 			else {
+				//3で割り切れる
 				if ((i%3)==0) {
 					System.out.println("Fizz");
 				}
+				//5で
 				else if ((i%5)==0) {
 					System.out.println("Buzz");
 				}
